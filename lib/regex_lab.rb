889b96fcc2bb19f9e6afd8a62bcdcead95c95ac5
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-19 17:04:02 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,8 +11,16 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match?(/\A[A-Z].+\W$/)
 end
+
+# I stepped on a Corn Flake, now I'm a cereal killer. 
+# 
+# i wondered why the baseball was getting bigger. Then it hit me.
+# 
+# Did you hear about the guy whose whole left side was cut off? He's all right now
+# 
+# when fish are in schools, they sometimes take debate
 
 def valid_phone_number?(phone)
 
